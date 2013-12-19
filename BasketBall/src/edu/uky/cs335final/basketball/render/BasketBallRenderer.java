5cae70068299f2e375e27e128dbbc40f61d9747f
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-12-19 15:07:49 -0500

    Add new code

--- BasketBall/src/edu/uky/cs335final/basketball/render/BasketBallRenderer.java
+++ BasketBall/src/edu/uky/cs335final/basketball/render/BasketBallRenderer.java
@@ -16,7 +16,10 @@ import edu.uky.cs335final.basketball.model.goal.Goal;
 import edu.uky.cs335final.basketball.model.goal.GoalFactory;
 import edu.uky.cs335final.basketball.shader.OpenGLProgram;
 import edu.uky.cs335final.basketball.shader.OpenGLProgramFactory;
+import edu.uky.cs335final.basketball.shader.SkyboxShaderProgram;
 import edu.uky.cs335final.basketball.shader.TextureUtils;
+import edu.uky.cs335final.basketball.skybox.Skybox;
+import edu.uky.cs335final.basketball.util.TextureHelper;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -25,269 +28,299 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static edu.uky.cs335final.basketball.collision.CollisionDetector.*;
-
 import static android.opengl.GLES20.*;
 import static android.opengl.Matrix.*;
 
 public class BasketBallRenderer implements GLSurfaceView.Renderer {
 
-    // Define listener to allow other components to perform specific operations
-    // at different points in the lifecycle of the shot
-    public static interface ShotListener {
+	// Define listener to allow other components to perform specific operations
+	// at different points in the lifecycle of the shot
+	public static interface ShotListener {
 
-        // This is executed when a shot begins
-        public void onStart();
+		// This is executed when a shot begins
+		public void onStart();
 
-        // This is executed when a shot is first completed regardless of
-        // whether it was successful or not. This does NOT get executed at
-        // the end of a replay.
+		// This is executed when a shot is first completed regardless of
+		// whether it was successful or not. This does NOT get executed at
+		// the end of a replay.
 
-        public void onComplete();
+		public void onComplete();
 
-        // This is executed when a shot successfully enters the hoop.
-        public void onSuccess();
+		// This is executed when a shot successfully enters the hoop.
+		public void onSuccess();
 
-        // This is executed when a shot misses the hoop.
-        public void onFailure();
-    }
+		// This is executed when a shot misses the hoop.
+		public void onFailure();
+	}
 
-    // Define listener to allow other components to perform specific operations
-    // at different points of a replay event
-    public static interface ReplayListener {
+	// Define listener to allow other components to perform specific operations
+	// at different points of a replay event
+	public static interface ReplayListener {
 
-        // This is executed when a replay is first started
-        public void onStart();
+		// This is executed when a replay is first started
+		public void onStart();
 
-        // This is executed when the replay is finished
-        public void onStop();
-    }
+		// This is executed when the replay is finished
+		public void onStop();
+	}
 
-    private static final String TAG = BasketBallRenderer.class.getCanonicalName();
+	private static final String TAG = BasketBallRenderer.class
+			.getCanonicalName();
 
-    // The basketball's initial position is relative to the camera's position
-    private static final Vector DISPLACEMENT = new Vector(0f, -2f, -2.5f);
-    private static final float RADIUS = 1.0f;
+	// The basketball's initial position is relative to the camera's position
+	private static final Vector DISPLACEMENT = new Vector(0f, -2f, -2.5f);
+	private static final float RADIUS = 1.0f;
 
-    private final float[] viewMatrix = MatrixUtils.newMatrix();
-    private final float[] projectionMatrix = MatrixUtils.newMatrix();
+	private final float[] viewMatrix = MatrixUtils.newMatrix();
+	private final float[] projectionMatrix = MatrixUtils.newMatrix();
 
-    private final Context context;
-    private final Vector lightPosition = new Vector(0f, 10f, 0f);
+	private final Context context;
+	private final Vector lightPosition = new Vector(0f, 10f, 0f);
 
-    private Camera camera;
+	private Camera camera;
 
-    private BasketBall basketBall;
-    private Goal goal;
+	private BasketBall basketBall;
+	private Goal goal;
 
-    private List<Renderable> models;
+	private List<Renderable> models;
 
-    private boolean shotInProgress;
-    private boolean shotFinished;
+	private boolean shotInProgress;
+	private boolean shotFinished;
 
-    private boolean replayInProgress;
-    private boolean gameOver;
+	private boolean replayInProgress;
+	private boolean gameOver;
 
-    private ShotListener shotListener;
-    private ReplayListener replayListener;
+	private ShotListener shotListener;
+	private ReplayListener replayListener;
 
-    private Plane floor;
+	private Plane floor;
 
-    public BasketBallRenderer(Context context, Camera camera) {
-        Log.d(TAG, "Instantiating renderer");
+	private SkyboxShaderProgram skyboxProgram;
+	private Skybox skybox;
 
-        this.context = context;
-        this.camera = camera;
+	private int skyboxTexture;
+	// for rotation of sky box
+	public int yRotation = -10;
+	public int xRotation = 90;
 
-        this.models = new ArrayList<Renderable>();
+	public BasketBallRenderer(Context context, Camera camera) {
+		Log.d(TAG, "Instantiating renderer");
 
-        Vector floorNormal = new Vector(0, 1, 0);
-        Vector pointOnFloor = new Vector(0, 0, 0);
+		this.context = context;
+		this.camera = camera;
 
-        this.floor = new Plane(floorNormal, pointOnFloor);
-    }
+		this.models = new ArrayList<Renderable>();
 
-    public boolean isReplayInProgress() {
-        return replayInProgress;
-    }
+		Vector floorNormal = new Vector(0, 1, 0);
+		Vector pointOnFloor = new Vector(0, 0, 0);
 
-    public boolean isGameOver() {
-        return gameOver;
-    }
+		this.floor = new Plane(floorNormal, pointOnFloor);
+	}
 
-    public void setShotListener(ShotListener shotListener) {
-        this.shotListener = shotListener;
-    }
+	public boolean isReplayInProgress() {
+		return replayInProgress;
+	}
 
-    public void setReplayListener(ReplayListener replayListener) {
-        this.replayListener = replayListener;
-    }
+	public boolean isGameOver() {
+		return gameOver;
+	}
 
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        Log.d(TAG, "Creating surface");
+	public void setShotListener(ShotListener shotListener) {
+		this.shotListener = shotListener;
+	}
 
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	public void setReplayListener(ReplayListener replayListener) {
+		this.replayListener = replayListener;
+	}
 
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
-
-        if(models.isEmpty())
-            createModels();
-    }
-
-    private void createModels() {
-        basketBall = createBasketBall();
-        models.add(basketBall);
-
-        goal = GoalFactory.create(context);
-        models.add(goal);
-    }
-
-    private BasketBall createBasketBall() {
-        Log.d(TAG, "Creating basketball");
-
-        OpenGLProgram program = OpenGLProgramFactory.create(context,
-                R.raw.texture_vertex_shader, R.raw.texture_fragment_shader);
-
-        Vector eye = camera.getPosition();
-        Vector position = new Vector(eye).add(DISPLACEMENT);
-
-        int texture = TextureUtils.loadTexture(context, R.drawable.basketball_texture);
-
-        return new BasketBall(position, RADIUS, program, texture);
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        Log.d(TAG, "Surface changed");
-        glViewport(0, 0, width, height);
-
-        final float aspectRatio = (float) width / height;
-        frustumM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1, 1, 1, 100);
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        Log.v(TAG, "Drawing frame");
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        Log.v(TAG, "Creating view matrix");
-        camera.createViewMatrix(viewMatrix);
-
-        Log.v(TAG, "Rendering models");
-        for(Renderable model : models) {
-            model.render(viewMatrix, projectionMatrix, lightPosition);
-        }
-
-        if(shotInProgress)
-            update();
-    }
-
-    private void update() {
-        Log.d(TAG, "Updating ball");
-        basketBall.update();
-
-        if(collidesWithBackboard()) {
-            Log.d(TAG, "Ball hit the backboard");
-        }
-
-        if(collidesWithFloor()) {
-            Log.d(TAG, "Ball hit the floor");
-            setShotCompleteFlags();
-            checkForGameOver();
-        }
-    }
-
-    private boolean collidesWithBackboard() {
-        Sphere ball = basketBall.getSphere();
-        BoundingBox boundingBox = goal.getBackboardBoundingBox();
-
-        return CollisionDetector.intersectsWith(boundingBox, ball);
-    }
-
-    private boolean collidesWithFloor() {
-        return collidesWithPlane(floor);
-    }
-
-    private boolean collidesWithPlane(Plane plane) {
-        Sphere ball = basketBall.getSphere();
-        CollisionResult result = checkSpherePlaneCollision(ball, plane);
-        return result.equals(CollisionResult.INTERSECTS);
-    }
-
-    private void setShotCompleteFlags() {
-        shotInProgress = false;
-        shotFinished = true;
-    }
-
-    private void checkForGameOver() {
-        Log.d(TAG, "Checking for game over conditions");
-
-        if(replayInProgress) {
-            stopReplayListener();
-            setGameCompleteFlags();
-        }
-        else {
-            completeShotListener();
-        }
-    }
-
-    private void stopReplayListener() {
-        if(replayListener != null)
-            replayListener.onStop();
-    }
-
-    private void completeShotListener() {
-        if(shotListener != null)
-            shotListener.onComplete();
-    }
-
-    private void setGameCompleteFlags() {
-        Log.d(TAG, "Game complete");
-        replayInProgress = false;
-        gameOver = true;
-    }
-
-    public void startReplay() {
-        if(!gameOver) {
-            Log.d(TAG, "Preparing for replay shot");
-            setReplayFlags();
-            basketBall.resetTime();
-            startReplayListener();
-        }
-    }
-
-    private void startReplayListener() {
-        if(replayListener != null)
-            replayListener.onStart();
-    }
-
-    private void setReplayFlags() {
-        shotFinished = false;
-        shotInProgress = true;
-        replayInProgress = true;
-        gameOver = false;
-    }
-
-    public boolean canShoot() {
-        return !shotFinished && !shotInProgress && !replayInProgress && !gameOver;
-    }
-
-    public void shootBall(float speed) {
-        shotInProgress = true;
-
-        Vector direction = new Vector(camera.getDirection());
-        basketBall.setDirection(direction, speed);
-
-        startShotListener();
-    }
-
-    private void startShotListener() {
-        if(shotListener != null)
-            shotListener.onStart();
-    }
-
-    public void changeBallUpdateSpeed(float factor) {
-        basketBall.changeTimeFactor(factor);
-    }
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		Log.d(TAG, "Creating surface");
+
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+
+		skyboxProgram = new SkyboxShaderProgram(context);
+		skybox = new Skybox();
+
+		skyboxTexture = TextureHelper.loadCubeMap(context, new int[] {
+				R.drawable.left, R.drawable.right, R.drawable.bottom,
+				R.drawable.top, R.drawable.front, R.drawable.back });
+
+		if (models.isEmpty())
+			createModels();
+	}
+//move skybox
+	private void drawSkybox() {
+		setIdentityM(viewMatrix, 0);
+		rotateM(viewMatrix, 0, -yRotation, 1f, 0f, 0f);
+		translateM(viewMatrix, 0, 0, 0.5f, 0.2f);
+		rotateM(viewMatrix, 0, -xRotation, 0f, 1f, 0f);
+		multiplyMM(viewMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+		skyboxProgram.useProgram();
+		skyboxProgram.setUniforms(viewMatrix, skyboxTexture);
+		skybox.bindData(skyboxProgram);
+		skybox.draw();
+	}
+
+	private void createModels() {
+		basketBall = createBasketBall();
+		models.add(basketBall);
+
+		goal = GoalFactory.create(context);
+		models.add(goal);
+	}
+
+	private BasketBall createBasketBall() {
+		Log.d(TAG, "Creating basketball");
+
+		OpenGLProgram program = OpenGLProgramFactory.create(context,
+				R.raw.texture_vertex_shader, R.raw.texture_fragment_shader);
+
+		Vector eye = camera.getPosition();
+		Vector position = new Vector(eye).add(DISPLACEMENT);
+
+		int texture = TextureUtils.loadTexture(context,
+				R.drawable.basketball_texture);
+
+		return new BasketBall(position, RADIUS, program, texture);
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		Log.d(TAG, "Surface changed");
+		glViewport(0, 0, width, height);
+
+		final float aspectRatio = (float) width / height;
+		frustumM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1, 1, 1, 100);
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		Log.v(TAG, "Drawing frame");
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		Log.v(TAG, "Creating view matrix");
+		camera.createViewMatrix(viewMatrix);
+
+		Log.v(TAG, "Rendering models");
+		for (Renderable model : models) {
+			model.render(viewMatrix, projectionMatrix, lightPosition);
+		}
+
+		if (shotInProgress)
+			update();
+        drawSkybox();
+
+	}
+
+	private void update() {
+		Log.d(TAG, "Updating ball");
+		basketBall.update();
+
+		if (collidesWithBackboard()) {
+			Log.d(TAG, "Ball hit the backboard");
+		}
+
+		if (collidesWithFloor()) {
+			Log.d(TAG, "Ball hit the floor");
+			setShotCompleteFlags();
+			checkForGameOver();
+		}
+	}
+
+	private boolean collidesWithBackboard() {
+		Sphere ball = basketBall.getSphere();
+		BoundingBox boundingBox = goal.getBackboardBoundingBox();
+
+		return CollisionDetector.intersectsWith(boundingBox, ball);
+	}
+
+	private boolean collidesWithFloor() {
+		return collidesWithPlane(floor);
+	}
+
+	private boolean collidesWithPlane(Plane plane) {
+		Sphere ball = basketBall.getSphere();
+		CollisionResult result = checkSpherePlaneCollision(ball, plane);
+		return result.equals(CollisionResult.INTERSECTS);
+	}
+
+	private void setShotCompleteFlags() {
+		shotInProgress = false;
+		shotFinished = true;
+	}
+
+	private void checkForGameOver() {
+		Log.d(TAG, "Checking for game over conditions");
+
+		if (replayInProgress) {
+			stopReplayListener();
+			setGameCompleteFlags();
+		} else {
+			completeShotListener();
+		}
+	}
+
+	private void stopReplayListener() {
+		if (replayListener != null)
+			replayListener.onStop();
+	}
+
+	private void completeShotListener() {
+		if (shotListener != null)
+			shotListener.onComplete();
+	}
+
+	private void setGameCompleteFlags() {
+		Log.d(TAG, "Game complete");
+		replayInProgress = false;
+		gameOver = true;
+	}
+
+	public void startReplay() {
+		if (!gameOver) {
+			Log.d(TAG, "Preparing for replay shot");
+			setReplayFlags();
+			basketBall.resetTime();
+			startReplayListener();
+		}
+	}
+
+	private void startReplayListener() {
+		if (replayListener != null)
+			replayListener.onStart();
+	}
+
+	private void setReplayFlags() {
+		shotFinished = false;
+		shotInProgress = true;
+		replayInProgress = true;
+		gameOver = false;
+	}
+
+	public boolean canShoot() {
+		return !shotFinished && !shotInProgress && !replayInProgress
+				&& !gameOver;
+	}
+
+	public void shootBall(float speed) {
+		shotInProgress = true;
+
+		Vector direction = new Vector(camera.getDirection());
+		basketBall.setDirection(direction, speed);
+
+		startShotListener();
+	}
+
+	private void startShotListener() {
+		if (shotListener != null)
+			shotListener.onStart();
+	}
+
+	public void changeBallUpdateSpeed(float factor) {
+		basketBall.changeTimeFactor(factor);
+	}
 }
